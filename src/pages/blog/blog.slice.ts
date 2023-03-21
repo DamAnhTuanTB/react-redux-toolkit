8b3a7c0f8a96d0355102113b8f8380e668d813d4
Alan Nguyen <alan.nguyen@setel.com>
2023-03-21 17:29:08 +0700

    Doing

--- src/pages/blog/blog.slice.ts
+++ src/pages/blog/blog.slice.ts
@@ -1,17 +1,169 @@
-import { current, createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction, createAsyncThunk, AsyncThunk } from '@reduxjs/toolkit'
 // import { initalPostList } from 'constants/blog'
 import { Post } from 'types/blog.type'
 import http from 'utils/http'
 
+type GenericAsyncThunk = AsyncThunk<unknown, unknown, any>
+
+type PendingAction = ReturnType<GenericAsyncThunk['pending']>
+type RejectedAction = ReturnType<GenericAsyncThunk['rejected']>
+type FulfilledAction = ReturnType<GenericAsyncThunk['fulfilled']>
+
 interface BlogState {
   postList: Post[]
   editingPost: Post | null
+  loading: boolean
+  currentRequestId: undefined | string
 }
 
 const initialState: BlogState = {
   postList: [],
-  editingPost: null
+  editingPost: null,
+  loading: false,
+  currentRequestId: undefined
 }
+
+export const getPostList = createAsyncThunk('blog/getPostList', async (_, thunkApi) => {
+  const response = await http.get<Post[]>('posts', {
+    signal: thunkApi.signal
+  })
+  return response.data
+})
+
+export const addPost = createAsyncThunk('blog/addPost', async (body: Omit<Post, 'id'>, thunkApi) => {
+  try {
+    const response = await http.post<Post>('posts', body, {
+      signal: thunkApi.signal
+    })
+    return response.data
+  } catch (error: any) {
+    if (error.name === 'AxiosError' && error.response.status === 422) {
+      return thunkApi.rejectWithValue(error.response.data)
+    }
+    throw error
+  }
+})
+
+export const updatePost = createAsyncThunk(
+  'blog/updatePost',
+  async ({ postId, body }: { postId: string; body: Post }, thunkApi) => {
+    try {
+      const response = await http.put<Post>(`posts/${postId}`, body, {
+        signal: thunkApi.signal
+      })
+      return response.data
+    } catch (error: any) {
+      if (error.name === 'AxiosError' && error.response.status === 422) {
+        return thunkApi.rejectWithValue(error.response.data)
+      }
+      throw error
+    }
+  }
+)
+
+export const deletePost = createAsyncThunk('blog/deletePost', async (postId: string, thunkApi) => {
+  const response = await http.delete<Post>(`posts/${postId}`, {
+    signal: thunkApi.signal
+  })
+  return response.data
+})
+
+const blogSlice = createSlice({
+  name: 'blog',
+  initialState,
+  reducers: {
+    // deletePost: (state, action: PayloadAction<string>) => {
+    //   console.log('start', current(state))
+    //   const postId = action.payload
+    //   const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+    //   if (foundPostIndex !== -1) {
+    //     state.postList.splice(foundPostIndex, 1)
+    //   }
+    //   console.log('finish', current(state))
+    // },
+    startEditingPost: (state, action: PayloadAction<string>) => {
+      const postId = action.payload
+      const foundPost = state.postList.find((post) => post.id === postId) || null
+      state.editingPost = foundPost
+    },
+    cancelEditingPost: (state) => {
+      state.editingPost = null
+    }
+    // finishEditingPost: (state, action: PayloadAction<Post>) => {
+    //   const postId = action.payload.id
+    //   state.postList.some((post, index) => {
+    //     if (post.id === postId) {
+    //       state.postList[index] = action.payload
+    //       return true
+    //     }
+    //     return false
+    //   })
+    //   state.editingPost = null
+    // }
+    // addPost: {
+    //   reducer: (state, action: PayloadAction<Post>) => {
+    //     const post = action.payload
+    //     state.postList.push(post)
+    //   },
+    //   prepare: (post: Omit<Post, 'id'>) => ({
+    //     payload: {
+    //       ...post,
+    //       id: nanoid()
+    //     }
+    //   })
+    // }
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getPostList.fulfilled, (state, action) => {
+        state.postList = action.payload
+      })
+      .addCase(addPost.fulfilled, (state, action) => {
+        state.postList.push(action.payload)
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        const postId = action.payload.id
+        state.postList.some((post, index) => {
+          if (post.id === postId) {
+            state.postList[index] = action.payload
+            return true
+          }
+          return false
+        })
+        state.editingPost = null
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        const postId = action.meta.arg
+        const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
+        if (foundPostIndex !== -1) {
+          state.postList.splice(foundPostIndex, 1)
+        }
+      })
+      .addMatcher<PendingAction>(
+        (action) => action.type.endsWith('/pending'),
+        (state, action) => {
+          state.loading = true
+          state.currentRequestId = action.meta.requestId
+        }
+      )
+      .addMatcher<RejectedAction | FulfilledAction>(
+        (action) => action.type.endsWith('/rejected') || action.type.endsWith('/fulfilled'),
+        (state, action) => {
+          if (state.loading && state.currentRequestId === action.meta.requestId) {
+            state.loading = false
+            state.currentRequestId = undefined
+          }
+        }
+      )
+      .addDefaultCase((state, action) => {
+        // console.log(`action type: ${action.type}`, current(state))
+      })
+  }
+})
+
+export const { startEditingPost, cancelEditingPost } = blogSlice.actions
+const blogReducer = blogSlice.reducer
+export default blogReducer
 
 // export const addPost = createAction('blog/addPost', function (post: Omit<Post, 'id'>) {
 //   return {
@@ -34,87 +186,6 @@ const initialState: BlogState = {
 // export const startEditingPost = createAction<string>('/blog/startEditingPost')
 // export const cancelEditingPost = createAction('/blog/cancelEditingPost')
 // export const finishEditingPost = createAction<Post>('/blog/finishEditingPost')
-
-export const getPostList = createAsyncThunk('blog/getPostList', async (_, thunkApi) => {
-  const response = await http.get<Post[]>('posts', {
-    signal: thunkApi.signal
-  })
-  return response.data
-})
-
-export const addPost = createAsyncThunk('blog/addPost', async (body: Omit<Post, 'id'>, thunkApi) => {
-  console.log(body)
-  const response = await http.post<Post>('posts', body, {
-    signal: thunkApi.signal
-  })
-  return response.data
-})
-
-const blogSlice = createSlice({
-  name: 'blog',
-  initialState,
-  reducers: {
-    deletePost: (state, action: PayloadAction<string>) => {
-      console.log('start', current(state))
-      const postId = action.payload
-      const foundPostIndex = state.postList.findIndex((post) => post.id === postId)
-      if (foundPostIndex !== -1) {
-        state.postList.splice(foundPostIndex, 1)
-      }
-      console.log('finish', current(state))
-    },
-    startEditingPost: (state, action: PayloadAction<string>) => {
-      const postId = action.payload
-      const foundPost = state.postList.find((post) => post.id === postId) || null
-      state.editingPost = foundPost
-    },
-    cancelEditingPost: (state) => {
-      state.editingPost = null
-    },
-    finishEditingPost: (state, action: PayloadAction<Post>) => {
-      const postId = action.payload.id
-      state.postList.some((post, index) => {
-        if (post.id === postId) {
-          state.postList[index] = action.payload
-          return true
-        }
-        return false
-      })
-      state.editingPost = null
-    }
-    // addPost: {
-    //   reducer: (state, action: PayloadAction<Post>) => {
-    //     const post = action.payload
-    //     state.postList.push(post)
-    //   },
-    //   prepare: (post: Omit<Post, 'id'>) => ({
-    //     payload: {
-    //       ...post,
-    //       id: nanoid()
-    //     }
-    //   })
-    // }
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(getPostList.fulfilled, (state, action) => {
-        state.postList = action.payload
-      })
-      .addCase(addPost.fulfilled, (state, action) => {
-        console.log(111, action.payload)
-        state.postList.push(action.payload)
-      })
-      .addMatcher(
-        (action) => action.type.includes('cancel'),
-        (state) => {
-          console.log(current(state))
-        }
-      )
-      .addDefaultCase((state, action) => {
-        console.log(`action type: ${action.type}`, current(state))
-      })
-  }
-})
 
 // const blogReducer = createReducer(initalState, (builder) => {
 //   builder
@@ -217,9 +288,5 @@ const blogSlice = createSlice({
 //     console.log(state)
 //   }
 // )
-
-export const { deletePost, startEditingPost, cancelEditingPost, finishEditingPost } = blogSlice.actions
-const blogReducer = blogSlice.reducer
-export default blogReducer
 
 // export default blogReducer
